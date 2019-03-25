07eddec120cf1d3813a0fef73249b1525a6f685a
Soujanya Bhat <sousur95@gmail.com>
2019-03-25 17:27:20 -0500

    Update and rename DFS.py to Recursive_DFS.py

--- Recursive_DFS.py
+++ Recursive_DFS.py
@@ -1,30 +1,12 @@
 '''
-A simple code for DFS for directed and undirected edges.
+A recursive code for DFS for directed and undirected edges.
 '''
-
-#Creating a stack data structure
-class stack:
-    def __init__(self):
-        self.items=[]
-    def pop(self):
-        return self.items.pop(0)
-    def push(self,i):
-        return self.items.insert(0,i)
-    def top(self):
-        return self.items[0]
-    def size(self):
-        return len(self.items)
-
-#Global Declaration
-visited=[] 
-s=stack()
-       
+    
 #Definition of DFS 
 def DFS(graph,node):
     
     #If the node is not in the visited list, then enter the loop else go back to the previous recursive call
     if node not in visited:
-        s.push(node)
         visited.append(node)
         for n in graph[node]:
             DFS(graph,n)
