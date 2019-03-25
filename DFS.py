afd6f823826a7c6fbae93192f8ac24e1d25b7aeb
Soujanya Bhat <sousur95@gmail.com>
2019-03-25 17:08:26 -0500

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,3 +1,8 @@
+'''
+A simple code for DFS for directed and undirected edges.
+'''
+
+#Creating a stack data structure
 class stack:
     def __init__(self):
         self.items=[]
@@ -9,11 +14,15 @@ class stack:
         return self.items[0]
     def size(self):
         return len(self.items)
+
+#Global Declaration
 visited=[] 
 s=stack()
        
+#Definition of DFS 
 def DFS(graph,node):
     
+    #If the node is not in the visited list, then enter the loop else go back to the previous recursive call
     if node not in visited:
         s.push(node)
         visited.append(node)
@@ -21,10 +30,16 @@ def DFS(graph,node):
             DFS(graph,n)
     else:
         return
+    
+#Printing the the order of DFS 
 def printVisited():
     print(visited)    
         
 if __name__=="__main__":
+    '''
+    Declaration of the graph. When a node does not have any edge 
+    leading to another edge, please insert it as an empty list. For example "R":[]
+    '''
     graph = {
     'A' : ['S','B'],
     'B' : ['A'],
@@ -36,5 +51,6 @@ if __name__=="__main__":
     'H' : ['E','G'],
     'S' : ['A','C','G']
     }
+    #'A' is the root node
     DFS(graph,'A')
     printVisited()
